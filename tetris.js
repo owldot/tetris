8fa763dad19f69f05a4b1bdbde95077978acfbcd
Lana Dzyuban <svitlana.dzyuban@gmail.com>
2020-09-28 23:47:55 -0400

    Add styles for button start

--- tetris.js
+++ tetris.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const width = 10;
+  const grid = document.querySelector('.grid');
+  const scoreDisplay = document.querySelector('#score');
+  const startBtn = document.querySelector('#start-button');
+  let squares = Array.from(document.querySelectorAll('.grid div'))
 
 })

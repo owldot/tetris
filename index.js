b49e3726bc2ac398b502e115392e83a1e504e938
Lana Dzyuban <svitlana.dzyuban@gmail.com>
2020-10-09 00:49:57 -0400

    Add pause game functionality.  Prevent controls to be enabled when on pause. Start new game button; Add hint and copyright;

--- index.js
+++ index.js
@@ -4,14 +4,40 @@ const { Board } = require("./src/board.js");
 document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   const scoreDisplay = document.querySelector('#score');
-  // const startBtn = document.querySelector('#start-button');
+  const startBtn = document.querySelector('#start-button');
+  const pauseLabel = document.querySelector('#pauseLabel');
+  document.addEventListener('keydown', listenKeyMove);
+  document.addEventListener('keydown', listenKeyPause);
+  startBtn.addEventListener('click', startNewGame);
 
   let squares = Array.from(document.querySelectorAll('.grid div'))
   let tetris = new Tetris(10, 20);
 
-  let interval = setInterval(nextMoveDown.bind(this), 700);
-  document.addEventListener('keydown', keyPush);
-  function keyPush(event) {
+  let togglePause = false;
+  let interval = setInterval(nextMoveDown.bind(this), 600);
+
+  function listenKeyPause(event) {
+    switch (event.keyCode) {
+      case 27:
+        togglePause = !togglePause;
+        if (togglePause) {
+          pauseLabel.parentElement.classList.remove('hidden')
+          document.removeEventListener('keydown', listenKeyMove);
+          clearInterval(interval);
+        } else {
+          document.addEventListener('keydown', listenKeyMove);
+          pauseLabel.parentElement.classList.add('hidden')
+          interval = setInterval(nextMoveDown.bind(this), 700);
+        }
+        break;
+    }
+  }
+
+  function startNewGame() {
+    tetris = new Tetris(10, 20);
+  }
+
+  function listenKeyMove(event) {
     switch (event.keyCode) {
       case 37:
         tetris.moveLeft();
